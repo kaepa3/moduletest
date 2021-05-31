66494a6370cff5bf00b2f1e1656b1e7e895a94c5
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2021-05-31 10:23:37 +0900

    is trace

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from moduletest2 import mod
 
 if __name__ == '__main__':
-    print("-----start------")
+    print("-----hogehoge------")
     print(sys.version)
     mod.Hoge()
     mod.Call()
