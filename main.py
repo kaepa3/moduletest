12e2f7941925ef1faa9c1df160a31caa89bb99a6
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2021-05-31 10:44:29 +0900

    fix main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from moduletest2 import mod
 
 if __name__ == '__main__':
-    print("-----hogehoge------")
+    print("-----nanzo------")
     print(sys.version)
     mod.AtBB()
     mod.Call()
